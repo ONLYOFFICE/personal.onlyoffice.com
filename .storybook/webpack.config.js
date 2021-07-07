107f7bc7f43ba0f32c86e8f29234e19e9585bad4
Artem Tarasov <artem.tarasov@onlyoffice.com>
2021-07-07 13:37:19 +0300

    Updated config storybook for linaria

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -6,19 +6,32 @@ module.exports = ({ config }) => {
   const fileLoaderRule = rules.find((rule) => rule.test.test(".svg"));
   fileLoaderRule.exclude = /\.react.svg$/;
 
-  rules.push({
-    test: /\.react.svg$/,
-    use: [
-      {
-        loader: "@svgr/webpack",
-        options: {
-          svgoConfig: {
-            plugins: [{ removeViewBox: false }],
+  rules.push(
+    {
+      test: /\.react.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [{ removeViewBox: false }],
+            },
           },
         },
-      },
-    ],
-  });
+      ],
+    },
+    {
+      test: /\.(js)$/,
+      use: [
+        {
+          loader: "linaria/loader",
+          options: {
+            sourceMap: process.env.NODE_ENV !== "production",
+          },
+        },
+      ],
+    }
+  );
 
   config.plugins.push(
     new CopyPlugin({
