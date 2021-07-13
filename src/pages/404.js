7db96179e7ffe2eebb9af06e918ea625a329b7c4
Shudrik Nikolai <46368637+ShudrikNikolai@users.noreply.github.com>
2021-07-13 15:54:31 +0300

    404: added custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class Error404Page extends React.PureComponent {
+render(){
+    return (
+        <div>
+            404!!!
+        </div>
+
+    );
+  }
+};
+export default Error404Page;
