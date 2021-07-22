521038854aae8eb7cea1a364da17046e7d5f768c
Vyacheslav-Belov-OO <vyacheslav.belov@onlyoffice.com>
2021-07-22 16:08:07 +0300

    start, not finished

--- components/info-panel/index.js
+++ components/info-panel/index.js
@@ -1,13 +1,46 @@
 import React from "react";
 import PropTypes from "prop-types";
-import StyledInfoPanel from "./styled-info-panel";
+import {StyledInfoPanel, StyledInfoPanelText, StyledInfoPanelImg} from "./styled-info-panel";
+import Text from "../text/index";
+import Button from "../button/index";
 
 const InfoPanel = (props) => {
-  return <StyledInfoPanel {...props} />;
-};
+  
+  if(props.imgleft){
+    return(
+      <StyledInfoPanel
+
+      >
+        <StyledInfoPanelText>
+          <Text fontSize={props.fontSize} color={props.textColor}>{props.children}</Text>
+          <Button></Button>
+        </StyledInfoPanelText>
+        <StyledInfoPanelImg>
+          
+        </StyledInfoPanelImg>
+        
+        
+      </StyledInfoPanel>
+    )
+  } else {
+    return(
+      <StyledInfoPanel>
+        <StyledInfoPanelImg>          
+        </StyledInfoPanelImg>
+        <StyledInfoPanelText>         
+          <Text fontSize={props.fontSize} color={props.textColor}>{props.children}</Text>
+          <Button></Button>
+        </StyledInfoPanelText>        
+      </StyledInfoPanel>
+    )
+  } 
+ 
+}
 
 InfoPanel.propTypes = {};
 
 InfoPanel.defaultProps = {};
 
 export default InfoPanel;
+
+
--- components/info-panel/styled-info-panel.js
+++ components/info-panel/styled-info-panel.js
@@ -1,5 +1,39 @@
 import { styled } from "linaria/react";
 
-const StyledInfoPanel = styled.div``;
+const StyledInfoPanel = styled.div`
 
-export default StyledInfoPanel;
+    display: flex;
+    width: 1200px;
+    margin: 0 auto;
+    align-items: center;
+    margin: 0 auto;
+    width: 1120px;
+    margin-bottom: 32px;
+    min-height: 380px;
+    border: 1px solid #ccc;
+    box-shadow: 0px 7px 15px rgb(85 85 85 / 10%);
+    box-sizing: border-box;
+
+`;
+const StyledInfoPanelText = styled.div`
+    width:50%;
+    font-size: 22px;
+    letter-spacing: -0.02em;
+    line-height: 256px;
+    padding: 32px;
+    text-align: center;
+    width: 504px;
+`;
+
+const StyledInfoPanelImg = styled.div`
+    width:50%
+    background-position: bottom;
+    background-repeat: no-repeat;
+    background-size: contain;
+    height: 380px;
+    width: 550px;
+`;
+
+
+
+export {StyledInfoPanel, StyledInfoPanelText, StyledInfoPanelImg};
