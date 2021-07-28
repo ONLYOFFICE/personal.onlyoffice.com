b1a6436959ad8d04ea25cc5a95f99526085b2407
Artem Tarasov <artem.tarasov@onlyoffice.com>
2021-07-28 10:30:05 +0300

    fix components

--- src/pages/sub-components/footer-content/index.js
+++ src/pages/sub-components/footer-content/index.js
@@ -14,51 +14,64 @@ const currentYear = date.getFullYear();
 const FooterContent = (props) => {
   const { t, language, footerHomePage, ...rest } = props;
 
-  return(      
+  return (
     <StyledFooter {...rest}>
-        <div className="personal-footer">               
-            {footerHomePage &&
-                <ul className="personal-social-links">                             
-                    {Social.map((item, i) => 
-                        <li key={i}>
-                            <CustomLink 
-                                className={item.className} 
-                                href={item.href} 
-                                target={item.target}
-                                title={item.title}
-                                rel={item.rel}
-                                type={"external"}
-                            >                                          
-                                <IconButton 
-                                    iconName={item.src}                                                  
-                                    size={item.size}
-                                    grayed={item.filter}
-                                />
-                            </CustomLink>
-                        </li>
-                    )}                                   
-                </ul>
-            }
-            <div className="item-personal-footer">
-                <div className="personal-footer-links">
-                    <CustomLink className="personal-footer-link" color="#444444" href={`/Terms.aspx`}>{t("Terms of Service")}</CustomLink>
-                    <CustomLink color="#444444" href={`https://www.onlyoffice.com`} type={"external"}>{t("Corporate Use")}</CustomLink>
-                </div>
-                    <Text color="#444444" className="personal-footer_rights">
-                        {t("© Ascensio System SIA")} {currentYear}. {t("All rights reserved")}
-                    </Text>
-            </div> 
-        </div>                      
-    </StyledFooter>           
-    ); 
-}
+      <div className="personal-footer">
+        {footerHomePage && (
+          <ul className="personal-social-links">
+            {Social.map((item, i) => (
+              <li key={i}>
+                <CustomLink
+                  className={item.className}
+                  href={item.href}
+                  target={item.target}
+                  title={item.title}
+                  rel={item.rel}
+                  type={"external"}
+                >
+                  <IconButton
+                    iconName={item.src}
+                    size={item.size}
+                    grayed={item.filter}
+                  />
+                </CustomLink>
+              </li>
+            ))}
+          </ul>
+        )}
+        <div className="item-personal-footer">
+          <div className="personal-footer-links">
+            <CustomLink
+              className="personal-footer-link"
+              color="#444444"
+              href={`/Terms.aspx`}
+            >
+              {t("Terms of Service")}
+            </CustomLink>
+            <CustomLink
+              color="#444444"
+              href={`https://www.onlyoffice.com`}
+              type={"external"}
+            >
+              {t("Corporate Use")}
+            </CustomLink>
+          </div>
+          <Text color="#444444" className="personal-footer_rights">
+            {t("© Ascensio System SIA")} {currentYear}.{" "}
+            {t("All rights reserved")}
+          </Text>
+        </div>
+      </div>
+    </StyledFooter>
+  );
+};
 
 FooterContent.propTypes = {
-    footerHomePage: PropTypes.bool
-}
+  footerHomePage: PropTypes.bool,
+};
 
 FooterContent.defaultProps = {
-    footerHomePage: false
-}
-  
+  footerHomePage: false,
+};
+
 export default FooterContent;
