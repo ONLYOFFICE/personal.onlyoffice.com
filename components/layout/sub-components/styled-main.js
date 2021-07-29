28b3f02e793aaebfaefad27b7866808f9890f203
Shudrik Nikolai <46368637+ShudrikNikolai@users.noreply.github.com>
2021-07-29 15:03:26 +0300

    Update styled-main.js

--- components/layout/sub-components/styled-main.js
+++ components/layout/sub-components/styled-main.js
@@ -3,8 +3,6 @@ import { styled } from "linaria/react";
 const StyledMain = styled.main`
   min-height: 100%;
   height: 100%;
-  padding-bottom: 150px;
-  padding-top: 150px;
 `;
 
 export default StyledMain;
