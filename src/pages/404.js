05377013cb22fbf465bdacc64711a3ed56b034bb
julmaystrovich <Maystrovich.Julia@onlyoffice.com>
2021-08-05 11:38:22 +0300

    add error container and edit 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { graphql } from "gatsby";
+import { useTranslation } from "gatsby-plugin-react-i18next";
+import ErrorContainer from "../sub-components/error-container";
 
 
-const NotFoundPage = () => <>Not Found</>;
+const NotFoundPage = () => {
+    const {
+        t,
+        i18n: { language },
+      } = useTranslation();
+
+      return (
+          <ErrorContainer
+          headerText = "Error 404. Page not found"
+          bodyText = "This page was removed, renamed or doesn’t exist anymore."
+          buttonText = "Return to homepage"
+          buttonUrl = "/"
+           />
+      )
+};
 
 export default NotFoundPage;
 
+export const query = graphql`
+  query($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
