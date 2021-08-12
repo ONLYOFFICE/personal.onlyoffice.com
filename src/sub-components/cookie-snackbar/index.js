009a2b82c5a8ce9587d18103038ff3c3b77edd40
Artem Tarasov <artem.tarasov@onlyoffice.com>
2021-08-12 23:46:04 +0300

    CookieSnackbar: style fix

--- src/sub-components/cookie-snackbar/index.js
+++ src/sub-components/cookie-snackbar/index.js
@@ -55,7 +55,7 @@ const CookieSnackbar = ({ buttonLabel }) => {
             onClick={onSetCookie}
             minwidth="99px"
             isScale
-            padding="unset"
+            padding="0 10px"
           />
         </StyledCookieSnackbar>
       </Snackbar>
