5138eb68638e4390b3c551129354473881518e14
Artem Tarasov <artem.tarasov@onlyoffice.com>
2021-08-18 16:18:53 +0300

    helpers: added getConfirmHeader func

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -36,3 +36,33 @@ export function parseQueryParams(params) {
   const parsedParams = queryString.parse(params);
   return parsedParams;
 }
+
+function getObjectByLocation(location) {
+  if (!location.search || !location.search.length) return null;
+
+  const searchUrl = location.search.substring(1);
+  const object = JSON.parse(
+    '{"' +
+      decodeURIComponent(searchUrl)
+        .replace(/"/g, '\\"')
+        .replace(/&/g, '","')
+        .replace(/=/g, '":"') +
+      '"}'
+  );
+
+  return object;
+}
+
+export function getConfirmHeader(location) {
+  const { search } = location;
+
+  const queryParams = getObjectByLocation(location);
+  const url = location.pathname;
+  const posSeparator = url.lastIndexOf("/");
+  const type = url.slice(posSeparator + 1);
+  const confirmLinkData = Object.assign({ type }, queryParams);
+
+  const confirmHeader = `type=${confirmLinkData.type}&${search.slice(1)}`;
+
+  return confirmHeader;
+}
