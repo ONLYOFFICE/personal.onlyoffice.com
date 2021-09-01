052e9ca67cef45e37b88b001e8054319f20d6f5c
Artem Tarasov <artem.tarasov@onlyoffice.com>
2021-09-01 17:52:10 +0300

    config: added prefix for assets

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,23 +8,23 @@ module.exports = {
     siteUrl: "https://www.personal.onlyoffice.com",
     title: "personal-gatsby",
   },
-
+  assetPrefix: "site-assets",
   plugins: [
-    `gatsby-plugin-styled-components`,
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    { resolve: "gatsby-plugin-styled-components" },
+    { resolve: "gatsby-plugin-image" },
+    { resolve: "gatsby-plugin-sharp" },
+    { resolve: "gatsby-transformer-sharp" },
+    { resolve: "gatsby-plugin-gatsby-cloud" },
+    { resolve: "gatsby-plugin-react-helmet" },
+    { resolve: "gatsby-plugin-sitemap" },
     {
       resolve: "gatsby-plugin-manifest",
       options: {
         icon: "src/images/logo/favicon_general.png",
       },
     },
-    "gatsby-transformer-remark",
-    "gatsby-plugin-mdx",
+    { resolve: "gatsby-transformer-remark" },
+    { resolve: "gatsby-plugin-mdx" },
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -71,13 +71,13 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-fonts`,
+      resolve: "gatsby-plugin-google-fonts",
       options: {
         fonts: [`Open Sans:200,300,400,400i,500,600,700,800`],
       },
     },
     {
-      resolve: `gatsby-plugin-portal`,
+      resolve: "gatsby-plugin-portal",
       options: {
         key: "portal",
         id: "portal",
@@ -89,6 +89,13 @@ module.exports = {
         rule: {
           include: /\.react.svg$/,
         },
+      },
+    },
+
+    {
+      resolve: "gatsby-plugin-asset-path",
+      options: {
+        copyAssets: false,
       },
     },
   ],
