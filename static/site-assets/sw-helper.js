50f0094a2bcabc513d87f1f1b00e72d179671577
Artem Tarasov <artem.tarasov@onlyoffice.com>
2021-09-30 23:37:58 +0300

    sw-helper: fixed warrning develop mode

--- static/site-assets/sw-helper.js
+++ static/site-assets/sw-helper.js
@@ -3,41 +3,43 @@ import { Workbox } from "https://storage.googleapis.com/workbox-cdn/releases/6.3
 const registerSW = () => {
   const wb = new Workbox(`/sw.js`);
 
-  const showSkipWaitingPrompt = (event) => {
-    console.log(
-      `A new service worker has installed, but it can't activate` +
-        `until all tabs running the current version have fully unloaded.`
-    );
+  if (wb) {
+    const showSkipWaitingPrompt = (event) => {
+      console.log(
+        `A new service worker has installed, but it can't activate` +
+          `until all tabs running the current version have fully unloaded.`
+      );
 
-    function refresh() {
-      wb.addEventListener("controlling", () => {
-        localStorage.removeItem("sw_need_activation");
-        window.location.reload();
-      });
+      function refresh() {
+        wb.addEventListener("controlling", () => {
+          localStorage.removeItem("sw_need_activation");
+          window.location.reload();
+        });
 
-      wb.messageSkipWaiting();
-    }
-
-    try {
-      if (localStorage.getItem("sw_need_activation")) {
-        refresh();
-        return;
+        wb.messageSkipWaiting();
       }
 
-      localStorage.setItem("sw_need_activation", true);
-    } catch (e) {
-      console.error("showSkipWaitingPrompt", e);
-      refresh();
-    }
-  };
+      try {
+        if (localStorage.getItem("sw_need_activation")) {
+          refresh();
+          return;
+        }
 
-  wb.addEventListener("waiting", showSkipWaitingPrompt);
+        localStorage.setItem("sw_need_activation", true);
+      } catch (e) {
+        console.error("showSkipWaitingPrompt", e);
+        refresh();
+      }
+    };
 
-  wb.register()
-    .then((reg) => {
-      console.log("Successful service worker registration (site)", reg);
-    })
-    .catch((err) => console.error("Service worker registration failed", err));
+    wb.addEventListener("waiting", showSkipWaitingPrompt);
+
+    wb.register()
+      .then((reg) => {
+        console.log("Successful service worker registration (site)", reg);
+      })
+      .catch((err) => console.error("Service worker registration failed", err));
+  }
 };
 
 window.SW = {
