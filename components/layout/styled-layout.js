fd62924f1259b4fc36341eacc41ca484139d852d
Artem Tarasov <artem.tarasov@onlyoffice.com>
2022-01-25 10:33:26 +0300

    Components: Layout: fixed style of background image

--- components/layout/styled-layout.js
+++ components/layout/styled-layout.js
@@ -1,10 +1,12 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const StyledLayout = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
   background-color: transparent;
+  overflow: hidden !important;
+  max-width: 100vw;
 `;
 
 export default StyledLayout;
