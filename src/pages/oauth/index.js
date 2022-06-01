cd029418da9b5a7362278c43abda3d4482d4a536
Artem Tarasov <artem.tarasov@onlyoffice.com>
2022-06-02 01:24:31 +0300

    pages: oauth: added oauth page

---
+++ src/pages/oauth/index.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import Loader from "../../../components/loader";
+import styled from "styled-components";
+import { thirdPartyLogin } from "../../api";
+import { navigate } from "gatsby";
+import { redirectToMainPage, removeOAuthKey } from "../../helpers";
+
+const StyledOAuth = styled.div`
+  position: absolute;
+  width: min-content;
+  margin: auto;
+  left: 50%;
+  top: 30%;
+`;
+
+const OAuthPage = () => {
+  useEffect(() => {
+    const profile = localStorage.getItem("profile");
+
+    thirdPartyLogin(profile)
+      .then(() => {
+        redirectToMainPage();
+      })
+      .catch((e) => {
+        navigate("/", {
+          state: {
+            toastr: {
+              error: true,
+              isProviderError: true,
+            },
+          },
+        });
+      })
+      .finally(() => {
+        removeOAuthKey();
+      });
+  }, []);
+  return (
+    <StyledOAuth>
+      <Loader className="loader-oauth" />
+    </StyledOAuth>
+  );
+};
+
+export default OAuthPage;
